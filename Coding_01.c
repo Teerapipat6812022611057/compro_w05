8d4d3b947a12a38134a835eca239f8dd91bc9bfa
Teerapipat6812022611057 <mkimmy162@gmail.com>
2025-07-22 17:35:23 +0700

    Update Coding_01.c

--- Coding_01.c
+++ Coding_01.c
@@ -3,7 +3,7 @@
 int main() {
     char name = 'N'; //ตัวอักษรตัวแรกองชื่อ
     int age = 18; //อายุ
-    double weight = 110.5; //น้ำหนัก
+    double weight = 75.5; //น้ำหนัก
 
     printf("Student %c is %d years old.\n", name, age);
     printf("His weighs is %.1f kg.\n", weight);
